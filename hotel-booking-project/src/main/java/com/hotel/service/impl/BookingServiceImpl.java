182a1a74b63436e921569d14766d8d281f382c6b
Hilmi Ege Kara <hegekara48@gmail.com>
2024-11-14 14:33:18 +0300

    layers created

---
+++ hotel-booking-project/src/main/java/com/hotel/service/impl/BookingServiceImpl.java
@@ -0,0 +1,10 @@
+package com.hotel.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.hotel.service.IBookingService;
+
+@Service
+public class BookingServiceImpl implements IBookingService{
+
+}
