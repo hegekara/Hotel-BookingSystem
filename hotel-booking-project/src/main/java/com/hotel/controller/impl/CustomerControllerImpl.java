b94c9a5e6383906b2ba8efe637d8339bd92a7572
Hilmi Ege Kara <hegekara48@gmail.com>
2024-12-04 16:08:58 +0300

    CORS yapılandırılması yapıldı

--- hotel-booking-project/src/main/java/com/hotel/controller/impl/CustomerControllerImpl.java
+++ hotel-booking-project/src/main/java/com/hotel/controller/impl/CustomerControllerImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -22,6 +23,7 @@ import com.hotel.dto.user.DtoCustomerIU;
 import com.hotel.service.ICustomerService;
 
 @RestController
+@CrossOrigin
 @RequestMapping("rest/api/customer")
 public class CustomerControllerImpl implements ICustomerController{
 
--- hotel-booking-project/src/main/java/com/hotel/security/SecurityConfig.java
+++ hotel-booking-project/src/main/java/com/hotel/security/SecurityConfig.java
@@ -22,6 +22,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
+            .cors(cors -> cors.disable())
             .authorizeHttpRequests(auth -> 
                 auth.requestMatchers("/rest/api/customer/login",
                 "/rest/api/customer/register",
